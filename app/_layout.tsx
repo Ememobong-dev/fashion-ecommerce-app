8a35d086601a11ef8f86057afde0d20fe36b87d3
Olushola <ememobongumoren2@gmail.com>
2026-05-25 14:00:49 +0100

    splash screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,20 +6,32 @@ import {
   LibreCaslonText_700Bold,
   LibreCaslonText_400Regular_Italic,
 } from "@expo-google-fonts/libre-caslon-text";
- 
+
+import {
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_700Bold,
+  Montserrat_800ExtraBold,
+} from "@expo-google-fonts/montserrat";
+import { View } from "react-native";
+
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
     LibreCaslonText_400Regular,
     LibreCaslonText_700Bold,
     LibreCaslonText_400Regular_Italic,
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_700Bold,
+    Montserrat_800ExtraBold,
   });
 
   if (!fontsLoaded) return null;
-  
+
   return (
-     <Stack screenOptions={{
-      headerShown: false,
-     }}  />
-);
+    <View className="flex-1 font-montserrat">
+      <Stack screenOptions={{ headerShown: false }} />
+    </View>
+  );
 }
